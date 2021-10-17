46bd3e96921228ff695dd088f46996335896caf0
MattToegel <matthew.toegel@njit.edu>
2021-10-16 23:22:01 -0400

    added basic auth - also regenerated the project due to babel issues

--- src/main.js
+++ src/main.js
@@ -1,4 +1,29 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+// @ts-ignore
+import App from "./App.vue";
+import { initializeApp } from "firebase/app";
+import router from "./router";
+//import { getAuth, onAuthStateChanged } from "@firebase/auth";
 
-createApp(App).mount('#app')
+// TODO: Replace the following with your app's Firebase project configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyClJu7Kob8TM-jiTm9AtEaV0If462IVN5k",
+  authDomain: "vue-my-portfolio.firebaseapp.com",
+  projectId: "vue-my-portfolio",
+  storageBucket: "vue-my-portfolio.appspot.com",
+  messagingSenderId: "303380551017",
+  appId: "1:303380551017:web:7e9e03bf7fe57ef61e59f8",
+};
+
+initializeApp(firebaseConfig);
+
+let app;
+//TODO may not be needed?
+//const auth = getAuth();
+//onAuthStateChanged(auth, () => {
+if (!app) {
+  app = createApp(App);
+  app.use(router);
+  app.mount("#app");
+}
+//});
